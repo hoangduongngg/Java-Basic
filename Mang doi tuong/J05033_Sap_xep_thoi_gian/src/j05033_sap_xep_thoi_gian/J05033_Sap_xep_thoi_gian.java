1eaf6e9854a68820dc13095de3267b6020f86de2
Hoang Duong Nguyen <hoangduongnguyen.tech@gmail.com>
2021-12-06 23:46:19 +0700

    daily

---
+++ Mang doi tuong/J05033_Sap_xep_thoi_gian/src/j05033_sap_xep_thoi_gian/J05033_Sap_xep_thoi_gian.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j05033_sap_xep_thoi_gian;
+
+/**
+ *
+ * @author hoangduongngg
+ */
+public class J05033_Sap_xep_thoi_gian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello");
+    }
+    
+}
